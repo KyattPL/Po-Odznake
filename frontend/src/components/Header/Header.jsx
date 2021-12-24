1db9fecbda0b7286c206dc0667b7771d580c10a0
Kajetan Pynka <siema1234@gazeta.pl>
2021-12-24 01:15:45 +0100

    Changed color scheme, finished footer, added logo, responsiveness should work (for current components). Prepared Boxes for header for specific screen sizes.

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,16 +3,32 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 
 import "../../styles/Header/header.css";
+import Box from "@mui/material/Box";
+
+// Trzeba będzie rozwalić na pliki te boxy wszystkie najlepiej bo zaraz niezłe zagnieżdżenia będą
+// Trzeba ogarnąć jak zrobić te opcje przy profilu (do którego boxa wciepać albo jak zrobić flexa odwróconego)
+// Nwm jak zrobić te paseczki sexy między opcjami (takie co nie sięgają od góry do dołu tylko tak trochu pomiędzy)
 
 function Header() {
     return (
         <Grid item xs={12}>
-            <AppBar position="static">
+            <AppBar position="static" className="header-appbar">
                 <Toolbar disableGutters className="header-toolbar">
-                    <div>logo</div>
-                    <div>opcje-dla-zalogowanych</div>
-                    <div>opcje-strony</div>
-                    <div>ikona-konta</div>
+                    <Box sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }} className="header-logo-box">
+                        <img src="logo_jaspyn_tekst.png" alt="Logo of the website" />
+                    </Box>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        {/* tutaj jest dla za małego ekraniku */}
+                    </Box>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <img src="logo_jaspyn_tekst.png" alt="Logo of the website" />
+                    </Box>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {/* tutaj są opcje dla większego ekranku */}
+                    </Box>
+                    <Box sx={{ flexGrow: 0 }}>
+                        {/* tutaj będzie logo */}
+                    </Box>
                 </Toolbar>
             </AppBar>
         </Grid>
