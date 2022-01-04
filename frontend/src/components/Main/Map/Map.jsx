4f7f9481995f51ae0673c39b6bd58504a3b9398e
Kajetan Pynka <siema1234@gazeta.pl>
2022-01-04 01:00:57 +0100

    Map works!

--- frontend/src/components/Main/Map/Map.jsx
+++ frontend/src/components/Main/Map/Map.jsx
@@ -1,3 +1,5 @@
+import { memo } from "react";
+
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 
@@ -5,12 +7,18 @@ import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
 import "../../../styles/Main/Map/map.css";
 
+const center = {
+    lat: 50.7622801,
+    lng: 15.7392952,
+};
+
 function Map() {
     return (
         <Grid item xs={6} sm={8} md={9} xl={10}>
             <Box className="map-container">
-                <LoadScript googleMapsApiKey="AIzaSyC1pd6MG7vR8Y3TLc2hM01HqUxpoA8Zkko">
-                    <GoogleMap>
+                <LoadScript googleMapsApiKey="AIzaSyCkgdzygd0uCHq2ZlcefnSQ3zlND-rOAbk">
+                    <GoogleMap mapContainerStyle={{ width: '100%', height: '100%' }}
+                        center={center} zoom={10} options={{ mapTypeControl: false }}>
 
                     </GoogleMap>
                 </LoadScript>
@@ -19,4 +27,4 @@ function Map() {
     );
 }
 
-export default Map;
+export default memo(Map);
